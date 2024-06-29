61d35417e82fbd715636810680ca084c25c3b284
Rajkumar <u.rajkumar2001@gmail.com>
2024-06-30 02:31:30 +0530

    updated

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ import Resumeimg from '../assets/resume.jpg'
 
 const Resume = () => {
   const config = {
-    link:"https://pdflink.to/34427943/"
+    link:"https://pdflink.to/e733a2eb/"
   }
   return (
     <section className='flex flex-col  md:flex-row bg-secondary px-5' id='resume' >
